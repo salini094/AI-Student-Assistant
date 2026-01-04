7eccf714682bd3b07e6fd0dc803af74b7e841378
salini094 <shaluslal04@gmail.com>
2026-01-04 23:30:46 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function askQuestion() {
+  document.getElementById("output").innerHTML =
+    "<b>User:</b> When is the internal exam?<br><b>AI:</b> Your internal exam is scheduled next week.";
+}
+
+function uploadNotice() {
+  document.getElementById("output").innerHTML =
+    "<b>Uploading image...</b><br><b>AI Vision:</b> Extracted text: Internal exam starts Monday.";
+}
